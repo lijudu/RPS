5894824277542a70ad0fc8d224f4351c6b8a1f00
Lisa <mkoliju@gmail.com>
2022-06-29 15:23:10 -0700

    Create initial interface

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-//rock paper scissors game!!
+// create function compuerPlay that randomly returns rock, paper, or scissors 
+
+
+// write a function that play single round of RPS, takes two parameters 1) playerSelection 2) computerSelection and returns string that declares winner of round
+
+
+// write function called game() that calls playRound funciton inside to play 5 round game that keeps score and reports winner or loser at the end 
