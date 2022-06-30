7eb0408df03764880ab17ef0a8109eae143abf10
Lisa <mkoliju@gmail.com>
2022-06-30 15:37:45 -0700

    Show opponent selection

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 // Log player's selection as event listener
 // assign value 1=rock, 2=scissors, 3=paper to player selection 
 let selectRock = 1;
+let selectPaper = 2;
+let selectScissors = 3;
 
 let rockBTN = document.getElementById("rockBTN");
 let paperBTN = document.getElementById("paperBTN");
@@ -11,17 +13,40 @@ rockBTN.addEventListener("click", function(){
 });
 
 paperBTN.addEventListener("click", function (){
-    console.log("Paper");
+    console.log(selectPaper);
 });
 
 scissorsBTN.addEventListener("click", function(){
-    console.log("Scissors");
+    console.log(selectScissors);
 });
 
-// create function compuerPlay that randomly returns rock, paper, or scissors 
-// generate random number 1, 2, or 3
-// assign value 1=rock, 2=scissors, 3=paper
+// create function computerPlay that randomly returns rock, paper, or scissors 
+// generate random number 1 (rock), 2 (paper), or 3 (scissors) to computer's selection 
 // unhide value to show opponent selection
+let compRock = 1;
+let compPaper = 2;
+let compScissors = 3;
+
+
+function compSel() {
+    let ranNumb = Math.floor(Math.random() * 3) + 1;
+        if (ranNumb == 1) {
+            document.getElementById("compRock").style.display="block";
+            document.getElementById("compPaper").style.display="none";
+            document.getElementById("compScissors").style.display="none";
+        }
+        else if (ranNumb == 2) {
+            document.getElementById("compPaper").style.display="block";
+            document.getElementById("compRock").style.display="none";
+            document.getElementById("compScissors").style.display="none";
+            
+        }
+        else if (ranNumb ==3) {
+            document.getElementById("compScissors").style.display="block";
+            document.getElementById("compPaper").style.display="none";
+            document.getElementById("compRock").style.display="none";
+        }
+}
 
 
 // write a function that play single round of RPS: compare playerSelection and computerSelection and declare winner 
