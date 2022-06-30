32b53e51d7e43adc2378e04410f732714ccdd25f
Lisa <mkoliju@gmail.com>
2022-06-30 12:45:27 -0700

    Log player's selection

--- game.js
+++ game.js
@@ -1,26 +1,23 @@
 // Log player's selection as event listener
 // assign value 1=rock, 2=scissors, 3=paper to player selection 
-Array.from(document.getElementsByClassName("Rock"))
-.forEach(function(element) {
-    element.addEventListener("click", function () {
-        console.log("Rock");
-    });
+let selectRock = 1;
+
+let rockBTN = document.getElementById("rockBTN");
+let paperBTN = document.getElementById("paperBTN");
+let scissorsBTN = document.getElementById("scissorsBTN");
+
+rockBTN.addEventListener("click", function(){
+    console.log(selectRock);
 });
 
+paperBTN.addEventListener("click", function (){
+    console.log("Paper");
+});
 
-Array.from(document.getElementsByClassName("Paper"))
-    .forEach(function(element) {
-        element.addEventListener("click", function () {
-            console.log("Paper");
-        });
-    });
+scissorsBTN.addEventListener("click", function(){
+    console.log("Scissors");
+});
 
-Array.from(document.getElementsByClassName("Scissors"))
-    .forEach(function(element) {
-        element.addEventListener("click", function () {
-            console.log("Scissors");
-        });
-    });
 // create function compuerPlay that randomly returns rock, paper, or scissors 
 // generate random number 1, 2, or 3
 // assign value 1=rock, 2=scissors, 3=paper
