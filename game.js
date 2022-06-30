45e53fadb1fc01a437d0019db503e670297cc099
Lisa <mkoliju@gmail.com>
2022-06-29 21:57:28 -0700

    Console log player's selection

--- game.js
+++ game.js
@@ -1,7 +1,33 @@
+// Log player's selection as event listener
+// assign value 1=rock, 2=scissors, 3=paper to player selection 
+Array.from(document.getElementsByClassName("Rock"))
+.forEach(function(element) {
+    element.addEventListener("click", function () {
+        console.log("Rock");
+    });
+});
+
+
+Array.from(document.getElementsByClassName("Paper"))
+    .forEach(function(element) {
+        element.addEventListener("click", function () {
+            console.log("Paper");
+        });
+    });
+
+Array.from(document.getElementsByClassName("Scissors"))
+    .forEach(function(element) {
+        element.addEventListener("click", function () {
+            console.log("Scissors");
+        });
+    });
 // create function compuerPlay that randomly returns rock, paper, or scissors 
+// generate random number 1, 2, or 3
+// assign value 1=rock, 2=scissors, 3=paper
+// unhide value to show opponent selection
 
 
-// write a function that play single round of RPS, takes two parameters 1) playerSelection 2) computerSelection and returns string that declares winner of round
+// write a function that play single round of RPS: compare playerSelection and computerSelection and declare winner 
 
 
-// write function called game() that calls playRound funciton inside to play 5 round game that keeps score and reports winner or loser at the end 
+//play 5 rounds and declare winner at the end of round
